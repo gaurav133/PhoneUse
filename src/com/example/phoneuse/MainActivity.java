b9773e4bfd96a99741601bbb22931dc0a6b3153a
Gaurav Jain <gauravjain.dtu@gmail.com>
2015-05-08 23:27:49 +0530

    Change data type to long.

--- src/com/example/phoneuse/MainActivity.java
+++ src/com/example/phoneuse/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends Activity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		Log.d("anurag","MainActivity oncreate called");
         mContext = this;
         init();
     }
