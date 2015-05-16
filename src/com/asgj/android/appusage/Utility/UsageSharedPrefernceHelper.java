249e73a006040cff416abeff74e6cd0fd857e4d6
anuragsingla911 <singla_anurag911@yahoo.co.in>
2015-05-16 23:20:34 +0530

    shared pref for music info

--- src/com/asgj/android/appusage/Utility/UsageSharedPrefernceHelper.java
+++ src/com/asgj/android/appusage/Utility/UsageSharedPrefernceHelper.java
@@ -1,5 +1,9 @@
 package com.asgj.android.appusage.Utility;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -46,5 +50,43 @@ public class UsageSharedPrefernceHelper {
 		SharedPreferences prefs = context.getSharedPreferences(
 			      PREFERNCE_NAME, Context.MODE_PRIVATE);
 		return prefs.getBoolean("isServiceRunningWhileShutDown", false);
+	}
+	public static ArrayList<UsageInfo> getTotalInfoOfMusic(Context context){
+		SharedPreferences prefs = context.getSharedPreferences(
+			      PREFERNCE_NAME, Context.MODE_PRIVATE);
+		Set<String> mInfoList = prefs.getStringSet("music", null);
+		ArrayList<UsageInfo> mInfo = new ArrayList<>();
+		for (String s : mInfoList){
+			mInfo.add(getMusicInfo(s));
+		}
+		return mInfo;
+		
+	}
+	
+	public static void setTotalIntervalsOfMusic(Context context,UsageInfo info){
+		SharedPreferences prefs = context.getSharedPreferences(
+			      PREFERNCE_NAME, Context.MODE_PRIVATE);
+		Editor editor = prefs.edit();
+		Set<String> mInfoList = new HashSet<>();
+		if(prefs.contains("music")){
+			mInfoList.addAll(prefs.getStringSet("music", null));
+		}
+		mInfoList.add(getFormattedStringFromMusicInfo(info));
+		editor.putStringSet("musicinfo", mInfoList);
+		editor.commit();
+	}
+	
+	private static String getFormattedStringFromMusicInfo(UsageInfo info){
+		return info.getmIntervalStartTime() + "_" + info.getmIntervalEndTime() + "_"
+				+ info.getmIntervalDuration();
+	}
+	
+	private static UsageInfo getMusicInfo(String info){
+		StringTokenizer tokenizer = new StringTokenizer(info, "_");
+		UsageInfo infoIns = new UsageInfo();
+		infoIns.setmIntervalStartTime(Long.parseLong((String)tokenizer.nextElement()));
+		infoIns.setmIntervalEndTime(Long.parseLong((String)tokenizer.nextElement()));
+		infoIns.setmIntervalDuration(Long.parseLong((String)tokenizer.nextElement()));
+		return infoIns;
 	}
 }
