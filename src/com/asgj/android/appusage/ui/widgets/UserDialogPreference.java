debfaaeb9dac3ab68319b271253a3ba7318b19aa
anuragsingla911 <singla_anurag911@yahoo.co.in>
2015-06-30 22:11:58 +0530

    remove unwanted import

--- src/com/asgj/android/appusage/ui/widgets/UserDialogPreference.java
+++ src/com/asgj/android/appusage/ui/widgets/UserDialogPreference.java
@@ -12,8 +12,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
 import android.view.View;
