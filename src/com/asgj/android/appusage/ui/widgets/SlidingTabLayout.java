a88286699e3ea8cf7f218b1453e681da04433f31
Gaurav Jain <gauravjain.dtu@gmail.com>
2015-06-30 19:55:03 +0530

    Fix window content overlay issue, NPE fix in MusicListAdapter, UI issue in group title, align detail fragment in tablet

--- src/com/asgj/android/appusage/ui/widgets/SlidingTabLayout.java
+++ src/com/asgj/android/appusage/ui/widgets/SlidingTabLayout.java
@@ -68,8 +68,8 @@ public class SlidingTabLayout extends HorizontalScrollView {
     }
 
     private static final int TITLE_OFFSET_DIPS = 24;
-    private static final int TAB_VIEW_PADDING_DIPS = 16;
-    private static final int TAB_VIEW_TEXT_SIZE_SP = 10;
+    public static final int TAB_VIEW_PADDING_DIPS = 16;
+    public static final int TAB_VIEW_TEXT_SIZE_SP = 10;
 
     private int mTitleOffset;
 
